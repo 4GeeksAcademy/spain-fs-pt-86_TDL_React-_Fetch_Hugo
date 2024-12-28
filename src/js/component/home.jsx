34044fe67f9bd97b3228c7921eae1b99ddd31858
Hvaxquez <hugoferney54@gmail.com>
2024-12-28 13:31:24 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,105 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	
+	const [inputValue,setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+	const apiUrl = 'https://playground.4geeks.com/todo/users/hugo'
+    
+	 useEffect(() => {
+       getData()
+	 }, []);
+
+	const getData = () => {
+		fetch(apiUrl,{
+			headers: {
+				"accept": "application/json",
+			},
+		})
+		.then((response) => response.json())
+		.then((data) => setTodos(data.todos))
+		.catch((error) => console.error("Error fetching tasks:", error)); 
+
+	}
+		
+		
+		/* const updateTasksOnServer = (newTasks) => {
+			fetch(apiUrl, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(newTasks),
+			})	
+			.then((response) => response.json()) 
+			.then((data) => console.log("Tasks updated:", data)) 
+			.catch((error) => console.error("Error updating tasks:", error));
+		 }; */
+		 const agregartarea = () => {
+			fetch('https://playground.4geeks.com/todo/todos/hugo', {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+				 label:inputValue,
+				 is_done:false,
+				}),
+			})	
+			.then((response) => response.json()) 
+			.then((data) => {
+				console.log(data)
+				getData()
+				setInputValue("")
+			    
+			}) 
+			.catch((error) => console.error("Error updating tasks:", error));
+		 }
+		  const borrartarea = (id) => {
+			fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				
+			})	
+			 
+			.then(response => {
+				//return response.json()
+				getData()
+		  })
+			 
+			.catch((error) => console.error("Error updating tasks:", error));
+		 }
+		 
+	 
+		return (
+		<div className="container">
+			<h1>Todos:_{inputValue}</h1>
+			<ul>
+				<li>
+					<input
+					    type="text"
+						onChange={(e)=> setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyDown= {(e) => {
+							if (e.key === "Enter") {
+								agregartarea()
+								
+								
+
+							}
+						}}
+						placeholder="what do you neet to do?"></input>
+				</li>
+				{todos.map((item) => (
+					<li key={item.id}>
+					   {item.label} <button className="btn btn-danger" onClick={()=> borrartarea(item.id)}>Eliminar</button>
+					</li>))}
+			</ul>
+			<div>{todos.length}item{todos.length !== 1 ? 's' : ''} left</div>
+			
 		</div>
 	);
 };
